16388a9c41c40f466821ec68260eb671cd9ea540
Jimmy Joel P. Malqui <jpmjim@gmail.com>
2021-10-19 17:50:53 -0500

    Toggle del menú

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -4,9 +4,9 @@ import '@styles/ProductItem.scss';
 import addToCart from '@icons/bt_add_to_cart.svg'
 
 const ProductItem = () => {
-  const [cart, setCart] = useState('Hola');
+  const [cart, setCart] = useState([]);
   const handleClick = () => {
-    setCart('Hola Mundo');
+    setCart([]);
   }
 	return (
 		<div className="ProductItem">
@@ -19,7 +19,6 @@ const ProductItem = () => {
 				<figure onClick={handleClick} >
 					<img src={addToCart} alt="" />
 				</figure>
-        {cart}
 			</div>
 		</div>
 	);

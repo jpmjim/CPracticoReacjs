dad5404f305f29240afcffb1ff8cb30aa4010f76
Jimmy Joel P. Malqui <jpmjim@gmail.com>
2021-10-19 19:46:32 -0500

    Carrito de compras

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import '@styles/Login.scss';
+import yardSale from '@logos/logo_yard_sale.svg';
 
 const Login = () => {
   const form = useRef(null);
@@ -17,7 +18,7 @@ const Login = () => {
 	return (
 		<div className="Login">
 			<div className="Login-container">
-				<img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+				<img src={yardSale} alt="logo" className="logo" />
 				<form action="/" className="form" ref={form}>
 					<label htmlFor="email" className="label">Email address</label>
 					<input type="text" name="email" placeholder="platzi@example.cm" className="input input-email" />

054228016314fde82d13f61c5e3c33fc301330f3
Jimmy Joel P. Malqui <jpmjim@gmail.com>
2021-10-19 22:33:35 -0500

    solucion deploy

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-    publicPath: '/'
+		filename: 'CPracticoReacjs/bundle.js',
+    publicPath: '/CPracticoReacjs/'
 	},
   mode: 'development',
 	resolve: {
